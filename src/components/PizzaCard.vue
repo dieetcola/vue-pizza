fabdcf76d8c1b30bd3cb6ec542960e7ac73c8eb5
Irakli Svanidze <svanidzeee@gmail.com>
2024-07-02 13:06:06 +0300

    implemented search functionality

---
+++ src/components/PizzaCard.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { Pizza } from "@/types/Pizza";
+import type { PropType } from "vue";
+
+const props = defineProps({
+  pizza: {
+    type: Object as PropType<Pizza>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <article class="pizza--details-wrapper">
+    <img :src="pizza.image" :alt="pizza.title" height="200" width="300" />
+    <p>{{ pizza.title }}</p>
+    <p>{{ pizza.description }}</p>
+    <div class="pizza--inventory">
+      <div class="pizza--inventory-stock">Stock: {{ pizza.quantity || 0 }}</div>
+      <div class="pizza--inventory-price">$ {{ pizza.price }}</div>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+.pizza--details-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  border: 1px solid #ccc;
+  padding: 0 1rem;
+}
+
+img {
+  object-fit: cover;
+}
+
+.pizza--inventory {
+  display: flex;
+  gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+</style>
--- src/views/PizzasView.vue
+++ src/views/PizzasView.vue
@@ -1,12 +1,59 @@
 <script lang="ts" setup>
+import { useRoute, useRouter } from "vue-router";
+
+import PizzaCard from "../components/PizzaCard.vue";
 import { usePizzas } from "../composables/usePizzas.ts";
+import { useSearch } from "../composables/useSearch";
+import type { Pizza } from "../types/Pizza.ts";
+import { watch, type Ref, onUpdated} from "vue";
 
 const { pizzas } = usePizzas();
-console.log(pizzas);
+const router = useRouter();
+const route = useRoute();
+
+type PizzaSearch = {
+  search: Ref<string>;
+  searchResults: Ref<Pizza[]>;
+};
+
+const { search, searchResults }: PizzaSearch = useSearch({
+  items: pizzas,
+  defaultSearch: route.query?.search as string,
+});
+
+watch(search, (value, prevValue) => {
+  if (value === prevValue) return;
+  router.replace({ query: { search: value } });
+});
 </script>
 
 <template>
-  <div>1</div>
+  <div class="pizzas-view--container">
+    <h1>Pizzas</h1>
+    <input v-model="search" placeholder="Search for a pizza" />
+    <ul>
+      <li v-for="pizza in searchResults" :key="pizza.id">
+        <PizzaCard :pizza="pizza" />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style lang=""></style>
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.pizzas-view--container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+}
+</style>
